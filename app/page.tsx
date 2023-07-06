d029315135bd7fe1066b52e706bea9aee4b0009b
Mitchell Moore <mmoorejr24@gmail.com>
2023-07-06 15:51:16 -0700

    Product List on Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,16 @@
-export default function Home() {
+type Product = {
+  id?: string;
+  product: string;
+  price: string;
+};
+
+async function getProducts(): Promise<Array<Product>> {
+  const res = await fetch(`${process.env.productApi}`, { cache: "no-cache" });
+  return res.json();
+}
+
+export default async function Home() {
+  const products = await getProducts();
   return (
     <>
       <header className="flex flex-col justify-center items-center w-full bg-gray-500">
@@ -8,15 +20,18 @@ export default function Home() {
         </div>
       </header>
       <main className="w-full">
-        <div className="flex justify-center items-center mt-24">
-          <form className="flex gap-4" action="">
+        <div className="flex max-sm:flex-col justify-center items-center mt-8">
+          <h2 className="font-bold text-5xl p-5 text-center">
+            List of Products
+          </h2>
+          <form className="flex max-sm:flex-col gap-4 max-sm:mb-4" action="">
             <input
-              className="border border-gray-300 p-2 rounded-md"
+              className="border border-gray-300 p-2 rounded-md max-sm:max-w-sm"
               placeholder="Enter Product name..."
               type="text"
             />
             <input
-              className="border border-gray-300 p-2 rounded-md"
+              className="border border-gray-300 p-2 rounded-md max-sm:max-w-sm"
               placeholder="Enter Product price..."
               type="text"
             />
@@ -24,6 +39,19 @@ export default function Home() {
               Add Product
             </button>
           </form>
+        </div>
+
+        <div className="w-full flex justify-center">
+          <div className="max-w-5xl border-2 flex flex-wrap gap-5 justify-center items-center mb-2">
+            {products.map((p) => (
+              <div key={p.id} className="my-2">
+                <div className="bg-slate-800 text-slate-50 p-4 flex flex-col content-center items-center">
+                  <p className="text-xl">{p.product}</p>
+                  <p>{p.price}</p>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </main>
     </>
--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    productApi: "https://64a73357096b3f0fcc812948.mockapi.io/products",
+  },
   experimental: {
     serverActions: true,
   },
